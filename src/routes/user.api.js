869b496fa0c87171b99a7149d16beb5df6d1a34d
MomenNano <momenslah1998@gmail.com>
2022-03-03 00:02:32 +0300

    refactor user routes

--- src/routes/user.api.js
+++ src/routes/user.api.js
@@ -1,55 +1,81 @@
-const User = require("../models/User");
-const Portfolio = require("../models/Portfolio");
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const router = express.Router();
+const express = require('express')
+// const jwt = require('jsonwebtoken')
+const router = express.Router()
 
-router.post("/addPortfolio", async (req, res) => {
+const User = require('../models/User')
+const Portfolio = require('../models/Portfolio')
+
+router.post('/addPortfolio', async (req, res) => {
   try {
-    const { user } = req.user;
-    if (user.role === 0) {
-      const { screenshots, details } = req.body;
-      const portfolio = new Portfolio({
-        screenshots,
-        details,
-      });
-      await portfolio.save();
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $set: { portfolio } },
-        { new: true }
-      ).populate("portfolio");
-      const token = jwt.sign({ user: updatedUser }, "SECRET");
+    const { user } = req.user
 
-      res.status(200).json({ updatedUser, token });
-    } else {
-      res.status(500).send("only freelancer has a portfolio");
+    if (user.role !== 0) {
+      res.status(500).send('only freelancer has a portfolio')
     }
+
+    const { screenshots, details } = req.body
+
+    const portfolio = new Portfolio.create({
+      screenshots,
+      details
+    })
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $set: { portfolio } },
+      { new: true }
+    ).populate('portfolio')
+    // const token = jwt.sign({ user: updatedUser }, 'SECRET')
+
+    res.status(200).json({ updatedUser })
   } catch (error) {
-    console.log(error);
-    res.status(500).send("server error addPortfolio");
+    console.error(error)
+    res.status(500).send("server error addPortfolio")
   }
-});
+})
+
 ////////////////////////////////////////////////////////////////
-router.post("/deletePortfolio", async (req, res) => {
+router.delete('/portfolio', async (req, res) => {
   try {
-    const { user } = req.user;
-    if (user.role === 0) {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $set: { portfolio: null } },
-        { new: true }
-      );
-      const token = jwt.sign({ user: updatedUser }, "SECRET");
-
-      res.status(200).json({ updatedUser, token });
-    } else {
-      res.status(500).send("only freelancer has a portfolio");
+    const { user } = req.user
+    if (user.role !== 0) {
+      res.status(500).send('only freelancer has a portfolio')
     }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $set: { portfolio: null } },
+      { new: true }
+    )
+    // const token = jwt.sign({ user: updatedUser }, 'SECRET')
+
+    res.status(200).json({ updatedUser })
   } catch (error) {
-    console.log(error);
-    res.status(500).send("server error deletePortfolio");
+    console.error(error)
+    res.status(500).send('server error deletePortfolio')
   }
-});
-module.exports = router;
+})
+
+router.put('/portfolio', async (req, res) => {
+  try {
+    const { user } = req.user
+    if (user.role !== 0) {
+      res.status(500).send('only freelancer has a portfolio')
+    }
+
+    const portfolioInfo = req.body
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $set: { portfolio: portfolioInfo } },
+      { new: true }
+    )
+    // const token = jwt.sign({ user: updatedUser }, 'SECRET')
+
+    res.status(200).json({ updatedUser })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('server error update portfolio')
+  }
+})
+module.exports = router
