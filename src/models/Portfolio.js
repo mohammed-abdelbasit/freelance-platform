2bc18794dc91d5714306c30c12c94b0cdde89153
mohammed-abdelbasit <mohbasit@gmail.com>
2022-03-09 08:38:46 +0200

    commit

--- src/models/Portfolio.js
+++ src/models/Portfolio.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
-
 const portfolioSchema = new Schema({
-    screenshots:[String],
-    details:String
-})
-const Portfolio = mongoose.model('Portfolio',portfolioSchema);
-
+  screenshots: [String],
+  title: String,
+  details: String,
+});
+const Portfolio = mongoose.model("Portfolio", portfolioSchema);
 
 module.exports = Portfolio;
 ///////////////////////////////////////////////////////////
--- src/routes/job.api.js
+++ src/routes/job.api.js
@@ -173,7 +173,7 @@ router.post("/clientCheckDelieverable", async (req, res) => {
     res.status(500).send("server error checkDelieverable");
   }
 });
-
+////////////////////////////////////////////////////////////////
 router.post("/freelancerCheckDelieverable", async (req, res) => {
   try {
     const { user } = req.user;
