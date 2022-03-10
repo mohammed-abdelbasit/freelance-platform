7599b421c45358683498f51a61ebfd391dbeb8ba
mohammed-abdelbasit <mohbasit@gmail.com>
2022-03-10 17:12:42 +0200

    serve html files with express and add login and signup script

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
 const auth = require("./middlewares/auth");
 const OpenUserRoutes = require("./routes/openUser.api");
 const UserRoutes = require("./routes/user.api");
@@ -7,13 +10,16 @@ const JobsRoutes = require("./routes/job.api");
 mongoose
   .connect("mongodb://127.0.0.1:27017/test", { useNewUrlParser: true })
   .then(() => console.log("connect to mongo"))
-  .catch((error) => console.log('faild to connect to database: ', error));
+  .catch((error) => console.log("faild to connect to database: ", error));
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.listen(4000, () => console.log("server is running on port 4000"));
+app.use(express.static(__dirname + "/front"));
 
 app.use("/user", OpenUserRoutes);
 
